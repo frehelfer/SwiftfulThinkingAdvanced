a80b41734e3f0cc427f71c4a3decac8e89acaa8c
Frédéric Helfer <fre.helfer@gmail.com>
2023-03-01 18:55:48 -0300

    Initial Commit

---
+++ SwiftfulThinkingAdvanced/SwiftfulThinkingAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedApp.swift
+//  SwiftfulThinkingAdvanced
+//
+//  Created by Frédéric Helfer on 01/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
